3dfa33b9201a1cd48c7a4808001ce898fa943f3f
Hiroshi Kawai <kawai@primalbase.jp>
2016-12-28 13:11:25 +0900

    update forms factory contracts.

--- src/Contracts/Forms/Factory.php
+++ src/Contracts/Forms/Factory.php
@@ -1,11 +1,7 @@
 <?php namespace Eyewill\TucleCore\Contracts\Forms;
 
-use Eyewill\TucleCore\Http\Presenters\ModelPresenter;
-
 interface Factory
 {
-  public function make(ModelPresenter $presenter);
-  public function getDefaultValue(ModelPresenter $presenter, $model = null);
   public function getAttributeNames();
   public function getName();
   public function getRequired();
@@ -13,6 +9,4 @@ interface Factory
   public function getHelp();
   public function getAttributes();
   public function getClass();
-  public function isPosition($position);
-
 }
