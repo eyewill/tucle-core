501b9909c530de872d5969c8b588c6fe0cf17c3d
Hiroshi Kawai <kawai@primalbase.jp>
2017-02-02 12:49:54 +0900

    supported role

--- src/Http/Presenters/TuclePresenter.php
+++ src/Http/Presenters/TuclePresenter.php
@@ -10,16 +10,35 @@ class TuclePresenter
     $modules = config('tucle.modules', []);
     foreach ($modules as $module)
     {
-      $presenter = app('App\\Http\\Presenters\\'.studly_case($module).'Presenter');
-      $url = $presenter->route('index');
-      $label = $presenter->getPageTitle();
-      $html.= '<li>';
-      $html.= '<a href="'.$url.'">';
-      $html.= e($label);
-      $html.= '</a>';
-      $html.= '</li>';
+      if (is_array($module))
+      {
+        $name = array_get($module, 'name');
+        $role = array_get($module, 'role');
+        if (app(\Illuminate\Contracts\Auth\Access\Gate::class)->allows($role))
+        {
+          $html.= $this->renderMenu($name);
+        }
+        continue;
+      }
+
+      $html.= $this->renderMenu($module);
     }
 
     return new HtmlString($html);
+  }
+
+  protected function renderMenu($name)
+  {
+    $html = '';
+    $presenter = app('App\\Http\\Presenters\\'.studly_case($name).'Presenter');
+    $url = $presenter->route('index');
+    $label = $presenter->getPageTitle();
+    $html.= '<li>';
+    $html.= '<a href="'.$url.'">';
+    $html.= e($label);
+    $html.= '</a>';
+    $html.= '</li>';
+
+    return $html;
   }
 }
