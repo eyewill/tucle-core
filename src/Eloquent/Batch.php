3617bfe53261500e9e6484388828cdc9c5e5b860
Hiroshi Kawai <kawai@primalbase.jp>
2017-02-16 18:07:27 +0900

    add transaction and force option to Batch

--- src/Eloquent/Batch.php
+++ src/Eloquent/Batch.php
@@ -1,5 +1,6 @@
 <?php namespace Eyewill\TucleCore\Eloquent;
 
+use Exception;
 use Illuminate\Database\Eloquent\Model;
 
 /**
@@ -10,36 +11,51 @@ trait Batch
 {
   /**
    * @param array $entries
+   * @param bool $force
    * @return int
    */
-  public static function batch($entries = [])
+  public static function batch($entries = [], $force = false)
   {
-    /** @var Model $model */
-
     $completes = 0;
-    foreach ($entries as $entry)
-    {
-      $type = array_get($entry, 'type');
-      $id = array_get($entry, 'id');
-      if ($type == 'delete')
+
+    app()->make('db')->transaction(function () use ($entries, $completes, $force) {
+
+      /** @var Model $model */
+
+      foreach ($entries as $entry)
       {
-        $model = static::find($id);
-        if ($model->delete())
+        $type = array_get($entry, 'type');
+        $id = array_get($entry, 'id');
+        if ($type == 'delete')
         {
-          $completes++;
+          $model = static::find($id);
+          if (!$model->delete())
+          {
+            if (!$force)
+              throw new Exception('deleting '.$model->getTables().' failure.');
+          }
+          else
+          {
+            $completes++;
+          }
+        }
+        elseif ($type == 'put')
+        {
+          $attributes = array_get($entry, 'attributes');
+          $model = static::find($id);
+          $model->fill($attributes);
+          if (!$model->save())
+          {
+            if (!$force)
+              throw new Exception('updating '.$model->getTables().' failure.');
+          }
+          else
+          {
+            $completes++;
+          }
         }
       }
-      elseif ($type == 'put')
-      {
-        $attributes = array_get($entry, 'attributes');
-        $model = static::find($id);
-        $model->fill($attributes);
-        if ($model->save())
-        {
-          $completes++;
-        }
-      }
-    }
+    });
 
     return $completes;
   }
