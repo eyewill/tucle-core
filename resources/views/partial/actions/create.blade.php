89198925b72fc9855c7b95d2844edf23d1c5a24c
Hiroshi Kawai <kawai@primalbase.jp>
2017-04-27 16:24:04 +0900

    exchange 'to index' and 'to create' in create action

--- resources/views/partial/actions/create.blade.php
+++ resources/views/partial/actions/create.blade.php
@@ -2,14 +2,16 @@
   <div class="col-xs-12">
     <div class="btn-toolbar pull-right">
 
+      <a href="{{ $presenter->route('index') }}" class="btn btn-default">
+        <span class="fa fa-list"></span>
+        一覧に戻る
+      </a>
+
       <a href="{{ $presenter->route('create') }}" class="btn btn-primary">
         <span class="fa fa-file-o"></span>
         作成
       </a>
-        <a href="{{ $presenter->route('index') }}" class="btn btn-default">
-          <span class="fa fa-list"></span>
-          一覧に戻る
-        </a>
+
     </div>
   </div>
 </div>
