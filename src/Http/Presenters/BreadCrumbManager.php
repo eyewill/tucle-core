1767125cdd7a2f66eb47064b375ca6854ef052e8
Hiroshi Kawai <kawai@primalbase.jp>
2017-05-08 17:11:39 +0900

    fix breadcrumb root url remove methods in TucleHomePresenter

--- src/Http/Presenters/BreadCrumbManager.php
+++ src/Http/Presenters/BreadCrumbManager.php
@@ -11,7 +11,7 @@ class BreadCrumbManager
     $this->setPrepends([
       [
         'label' => config('tucle.brand', 'TUCLE5'),
-        'url' => '/',
+        'url' => config('app.url'),
       ],
     ]);
   }
--- src/Http/Presenters/TucleHomePresenter.php
+++ src/Http/Presenters/TucleHomePresenter.php
@@ -1,46 +1,8 @@
 <?php namespace Eyewill\TucleCore\Http\Presenters;
 
-use Illuminate\Support\HtmlString;
-
 class TucleHomePresenter extends Presenter
 {
   protected $viewBase = 'tucle::home.';
-
-  protected $tableColumns = [
-    [
-      'label' => '機能',
-    ],
-  ];
-
-  protected $views = [
-    'partial' => [
-      'actions' => [
-        'index' => 'tucle::home.partial.actions.index',
-      ],
-      'datatables' => [
-        'make' => 'tucle::home.partial.datatables.make',
-        'actions' => [
-          'rows' => 'tucle::home.partial.datatables.actions.rows',
-        ],
-      ],
-    ],
-  ];
-
-  public function renderTableColumn($column, $presenter)
-  {
-    $html = '';
-    $html.= '<td>';
-    $html.= '<a href="'.$presenter->route('index').'">';
-    $html.= e($presenter->getPageTitle());
-    $html.= '</td>';
-
-    return new HtmlString($html);
-  }
-
-  public function renderTableRowClass()
-  {
-    return '';
-  }
 
   public function title()
   {
