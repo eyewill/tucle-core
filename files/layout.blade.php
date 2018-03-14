02d4d52aeef4a8480335f658bd81da8679bb6e55
primalbase <kawai@primalbase.jp>
2018-03-14 15:45:15 +0900

    add title method to TuclePresenter

--- files/layout.blade.php
+++ files/layout.blade.php
@@ -5,7 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>{{ config('tucle.brand', 'TUCLE5') }}</title>
+  <title>{{ $tucle->title() }}</title>
 
 @section('stylesheet')
   <!-- Fonts -->
